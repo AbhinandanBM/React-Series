40d3d35465d4207315f8545d86361ec4484190f4
Abhinandan B.M <bmabhi13@gmail.com>
2024-01-31 15:45:31 +0530

    custom react, jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from "./Chai"
 
 function App() {
 
+  const Username = "Abhi"
+
   return (
     <>
-    <Chai/>
-    <h1>Chai in vite</h1>
-    <p>Learning React from the best teacher</p>
+      <Chai />
+      <h1>Chai in vite {Username}</h1>
+      <p>Learning React from the best teacher</p>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Hi hello React</h1>
+    </div>
+  )
+}
+
+// const ReactElement ={
+//   type: 'a',
+//     props: {
+//         href: 'http://react.dev',
+//         target: '_blank',
+//     },
+//     children: "visit here to learn more"
+// }
+
+const anotherElement = (
+  <a href="http://react.dev" target='_blank'>Visit here</a>
+)
+
+
+const anotherUser = "Another User"
+
+const ReactElement = React.createElement(
+  'a',
+  { href: "http://react.dev", target: "_blank" },
+  "visit here to learn more",
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
